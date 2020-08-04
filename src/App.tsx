318442e9bd2855b5ad5b40c0087eb8ba996acb45
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-04 08:31:01 -0300

    feat: add all JSX structure

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ import Landing from './pages/Landing';
 import './assets/styles/global.css';
 
 const App: React.FC = () => (
-  <div>
-    <h1>Hello World</h1>
-    <Landing />
-  </div>
+  <Landing />
 );
 
 export default App;
