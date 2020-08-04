8012552248872f55f0bb22a7c8039494891c66cc
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-04 12:30:26 -0300

    fix: add Switch to Routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import TeachersList from './pages/TeachersList';
 import TeacherForm from './pages/TeacherForm';
 
 const Routes: React.FC = () => (
   <Router>
-    <Route exact path="/" component={Landing} />
-    <Route exact path="/estudar" component={TeachersList} />
-    <Route exact path="/dar-aulas" component={TeacherForm} />
+    <Switch>
+      <Route exact path="/" component={Landing} />
+      <Route path="/estudar" component={TeachersList} />
+      <Route path="/dar-aulas" component={TeacherForm} />
+    </Switch>
   </Router>
 );
 
