264559281ddf8d0ae0dd7aaed8b0de6826ac0596
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-04 09:23:16 -0300

    refactor: move header to PageHeader component

---
+++ src/components/PageHeader/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import backIcon from '../../assets/images/icons/back.svg';
+import logoImg from '../../assets/images/logo.svg';
+
+import './styles.css';
+
+interface IProps {
+  title: string;
+}
+
+const PageHeader: React.FC<IProps> = ({ title }) => (
+  <header className="page-header">
+    <div className="top-bar-container">
+      <Link to="/">
+        <img src={backIcon} alt="Voltar" />
+      </Link>
+      <img src={logoImg} alt="Proffy" />
+    </div>
+
+    <div className="header-content">
+      <strong>{title}</strong>
+    </div>
+  </header>
+);
+
+export default PageHeader;
