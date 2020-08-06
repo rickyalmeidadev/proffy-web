c98108d97520191b053829a8cb764950487047c0
rickyalmeidadev <ricky.almeida.dev@gmail.com>
2020-08-06 09:02:34 -0300

    feat: add TeacherForm page structure

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css';
 
 const TeacherForm: React.FC = () => (
   <div id="page-teacher-form" className="container">
@@ -7,6 +13,33 @@ const TeacherForm: React.FC = () => (
       title="Que incŕivel que você quer dar aulas."
       description="O primeiro passo é preencher esse formulário de inscrição"
     />
+
+    <main>
+      <fieldset>
+        <legend>Seus dados</legend>
+
+        <Input name="name" label="Nome completo" />
+        <Input name="avatar" label="Avatar" />
+        <Input name="whatsapp" label="WhatsApp" />
+      </fieldset>
+
+      <fieldset>
+        <legend>Sobre a aula</legend>
+
+        <Input name="subject" label="Matéria" />
+        <Input name="cost" label="Avatar" />
+        <Input name="whatsapp" label="Custo da sua hora por aula" />
+      </fieldset>
+
+      <footer>
+        <p>
+          <img src={warningIcon} alt="Aviso importante" />
+        </p>
+        <button type="button">
+          Salvar cadastro
+        </button>
+      </footer>
+    </main>
   </div>
 );
 
